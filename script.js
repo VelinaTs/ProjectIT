e32096a4cd3c38914917c460575ff1a66688d214
VelinaTs <velina.l.tsaneva.2023@elsys-bg.org>
2024-05-26 23:56:57 +0300

    1modified:   script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     var img1 = document.getElementById("img1");
     var img2 = document.getElementById("img2");
-    var img1 = document.getElementById("img3");
+    var img3 = document.getElementById("img3");
 
     function addHoverEffect(img) {
         img.addEventListener('mouseover', function() {
@@ -9,12 +9,12 @@ document.addEventListener('DOMContentLoaded', function() {
             img.style.transform = "scale(1.1)";
             img.style.boxShadow = "0px 8px 16px rgba(0, 0, 0, 0.2)";
         });
+
         img.addEventListener('mouseout', function() {
             img.style.transform = "scale(1)";
             img.style.boxShadow = "0px 4px 8px rgba(0, 0, 0, 0.1)";
         });
     }
-
     addHoverEffect(img1);
     addHoverEffect(img2);
     addHoverEffect(img3);
